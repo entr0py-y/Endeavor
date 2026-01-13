619f61930957921d15e2e3ae3e4c1fc07f0847b1
Eren Yeager <erenyeager@Erens-MacBook-Air.local>
2026-01-13 19:27:28 +0530

    Simplify ChromaticAberration offset type

--- components/core/WebGLScene.tsx
+++ components/core/WebGLScene.tsx
@@ -241,7 +241,7 @@ export default function WebGLScene() {
                         blendFunction={BlendFunction.ADD}
                     />
                     <ChromaticAberration
-                        offset={[0.0005, 0.0005] as [number, number]}
+                        offset={[0.0005, 0.0005]}
                         blendFunction={BlendFunction.NORMAL}
                     />
                 </EffectComposer>
